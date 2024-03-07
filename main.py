e24ffc0cd911b655b37b3352a4b4dad3977981c5
WalterHenri <walterdoideira5438@gmail.com>
2024-03-07 11:51:48 -0300

    centra           lizado

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-import threading
 import time
 
 class ClickSpeedTest:
@@ -7,20 +6,24 @@ class ClickSpeedTest:
         self.root = root
         self.root.title("Teste de Velocidade de Clique")
         self.root.geometry("800x600")
-        
+
         self.clicks = 0
         self.start_time = 0
         self.is_running = False
-        
+
+        # Lista de cores para o botão
+        self.colors = ["#0000aa", "#00aa00", "#aa0000", "#0000aa", "#00aa00", "#aa0000"]
+
         self.result_label = tk.Label(self.root, text=f"Você clicou {self.clicks} vezes.", font=("Arial", 20))
-        # Increase the font size and padding to make the button bigger
-        self.click_button = tk.Button(self.root, text="Clique aqui!", command=self.start_test, bg="#0000aa", font=("Arial", 30), padx=100, pady=100)
+        self.click_button = tk.Button(self.root, text="Clique aqui!", command=self.start_test, bg="#0000aa",
+                                      font=("Arial", 30), padx=100, pady=100)
         self.timer_label = tk.Label(self.root, text="Tempo restante: 5", font=("Arial", 20))
 
-        self.result_label.grid(row=0,column=0)
-        self.click_button.grid(row=2,column=0)
-        self.timer_label.grid(row=1,column=0)
-    
+        # Centralizando os widgets
+        self.result_label.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
+        self.click_button.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
+        self.timer_label.pack(side=tk.BOTTOM, expand=True, fill=tk.BOTH)
+
     def start_test(self):
         if not self.is_running:
             self.clicks = 0
@@ -29,21 +32,23 @@ class ClickSpeedTest:
             self.click_button.config(state=tk.NORMAL)
             self.click_button.config(command=self.increment_clicks)
             self.update_timer()
-    
+
     def increment_clicks(self):
         if self.is_running:
             self.clicks += 1
+            # Mudar a cor do botão a cada clique
+            self.click_button.config(bg=self.colors[self.clicks % len(self.colors)])
             self.result_label.config(text=f"Você clicou {self.clicks} vezes.")
-            
+
     def update_timer(self):
         if self.is_running:
             elapsed_time = int(time.time() - self.start_time)
             remaining_time = 5 - elapsed_time
             if remaining_time <= 0:
                 self.is_running = False
-                
+
                 self.timer_label.config(text="Tempo esgotado!")
-                self.click_button.config(command=self.start_test)
+                self.click_button.config(command=self.start_test, bg="white") # Altera a cor do botão para branco quando o tempo esgota
             else:
                 self.timer_label.config(text=f"Tempo restante: {remaining_time}")
                 self.root.after(1000, self.update_timer) # Atualiza o timer a cada segundo
